5524268039aaf58c90f5e42ab3c57b11c5fd7a7b
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-19 15:35:50 -0400

    implements dropdown error message, converts createTweetElement to return jQuery element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,63 +1,87 @@
 /* eslint-disable no-undef */
+
 const createTweetElement = (tweetObject) => {
+
+  // Get tweet data by destructuring
   const { name, avatars, handle } = tweetObject.user;
   const { text } = tweetObject.content;
 
-  return `<article class="tweet">
-  <header>
-  <div>
-  <img src=${avatars} />
-  <span>${escapeText(name)}</span>
-  </div>
-  <address>${escapeText(handle)}</address>
-  </header>
-  <section>
-  ${escapeText(text)}
-  </section>
-  <footer>
-  <time class="timeago" >${timeago.format(tweetObject.created_at)}</time>
-  <span class="icons">
-  <i class="fa-solid fa-flag"></i>
-  <i class="fa-solid fa-retweet"></i>
-  <i class="fa-solid fa-heart"></i>
-  <div>1</div>
-  </span>
-  </footer>
-  </article>`;
+
+  // Create main tweet element
+  const $tweet = $('<article>').addClass('tweet');
+
+
+  // Create header element, appending image, username and handle elements
+  const $header = $('<header>');
+  const $image = $('<img>').attr('src', avatars);
+  const $userName = $('<span>').text(name);
+  const $div = $('<div>').append($image, $userName);
+  const $address = $('<address>').text(handle);
+  $header.append($div, $address);
+
+
+  // Create content element
+  const $content = $('<section>').text(text);
+
+
+  // Create footer element, appending timeSincePost and icon elements
+  const $footer = $('<footer>');
+
+  const $timeSincePost = $('<time>').addClass('timeago').text(timeago.format(tweetObject.created_at)); // global timeago.format method
+
+  const $icons = $('<span>').addClass('icons');
+  const $flagIcon = $('<i>').addClass('fa-solid fa-flag');
+  const $retweetIcon = $('<i>').addClass('fa-solid fa-retweet');
+  const $heartIcon = $('<i>').addClass('fa-solid fa-heart');
+  const $inexplicableOne = $('<div>').text('1');
+  $icons.append($flagIcon, $retweetIcon, $heartIcon, $inexplicableOne); // span contains icons
+
+  $footer.append($timeSincePost, $icons);
+
+
+  // Append header, content and footer elements to tweet
+  $tweet.append($header, $content, $footer);
+
+  return $tweet;
 };
 
-const escapeText = (string) => {
-  const textNode = document.createTextNode(string);
-  const div = document.createElement('div');
-  div.appendChild(textNode);
-  return div.innerHTML;
-};
 
 const renderTweets = (tweetsArray) => {
-  let markup = '';
-  for (const tweet of tweetsArray.reverse()) {
-    markup += createTweetElement(tweet);
+  const tweetContainer = $('#tweets'); // get container element once
+
+  for (const tweet of tweetsArray) {
+    tweetContainer.append(createTweetElement(tweet)); // append each tweet
   }
-  $('#tweets').append($(markup));
 };
 
 const loadTweets = () => {
-  $.get('/tweets', (res) => {
-    $('#tweets').empty();
-    renderTweets(res);
-  });
+  $.ajax({ url: '/tweets' })
+    .then((tweets) => {
+      $('#tweets').empty();
+      renderTweets(tweets.reverse()); // array needs to be reversed because the 'server-side' function sortNewestFirst() is implemented wrong; it actually sorts the tweetArray ascending by tweet.created_at, which would be oldest first (more milliseconds = newer).
+    });
 };
+
+const postTweet = (tweet) => {
+  $.ajax({
+    type: 'POST',
+    url: '/tweets',
+    data: tweet
+  })
+    .then(() => {
+      clearForm();
+      loadTweets();
+    });
+};
+
+const isVisible = ($element) => {
+  return $element.css('display') !== 'none';
+};
+
 
 const clearForm = () => {
   $('#tweet-text').val('');
   $('.counter').val(140);
-};
-
-const postTweet = (tweet) => {
-  $.post('/tweets', tweet, () => {
-    clearForm();
-    loadTweets();
-  });
 };
 
 $().ready(() => {
@@ -80,10 +104,27 @@ $().ready(() => {
     },
 
     errorPlacement: (error) => {
-      alert(error[0].innerText);
+      const message = error[0].innerText;
+      const errorElement = $('section.error');
+
+      if (isVisible(errorElement)) {
+        errorElement.slideUp(150, () => {
+          errorElement.find('span').text(message);
+          errorElement.slideDown(150, () => console.log('Down...'));
+        });
+      } else {
+        errorElement.find('span').text(message);
+        errorElement.slideDown(150, () => console.log('Down...'));
+      }
     },
 
     submitHandler: (form) => {
+      const errorElement = $('section.error');
+
+      if (isVisible(errorElement)) {
+        $('.error').slideUp(150);
+      }
+
       const text = $(form).serialize();
       postTweet(text);
     }
