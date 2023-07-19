64f6236410939eb0a23803a4844bf0f2bc5edd27
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-07-18 21:36:38 -0400

    adds form validation with jQuery.validate plugin

--- public/scripts/submit-handler.js
+++ public/scripts/submit-handler.js
@@ -1,6 +1,25 @@
 $().ready(() => {
-  $('.new-tweet').find('form').submit(function(e) {
-    e.preventDefault();
-    $.post('/tweets', $(this).serialize());
+  $('.new-tweet').find('form').validate({
+    onfocusout: false,
+    onkeyup: false,
+    onclick: false,
+    rules: {
+      text: {
+        required: true,
+        maxlength: 140
+      }
+    },
+    messages: {
+      text: {
+        required: "You can't tweet an empty tweet, you twit!",
+        maxlength: 'This tweet is twoo long!'
+      }
+    },
+    errorPlacement: (error) => {
+      alert(error[0].innerText);
+    },
+    submitHandler: (form) => {
+      $.post('/tweets', $(form).serialize());
+    }
   });
 });
